a753522fb90371434f74f584d8c641c627cea2e4
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-01-23 08:47:57 -0500

    use first name if applicable

--- src/pages/components/TimeLine.tsx
+++ src/pages/components/TimeLine.tsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import { api } from '../../utils/api'
-import Post from './Post'
+import React from "react";
+import { api } from "../../utils/api";
+import Post from "./Post";
 
 const TimeLine = () => {
-
   return (
     <div className="ml-auto flex w-5/6 flex-col bg-slate-600 text-center">
       <Post />
       <TimeLineFeed />
     </div>
   );
-}
+};
 
 function TimeLineFeed() {
-    const timeLinePost = api.user.getFeed.useQuery()
-    return (
-        <div>
-            {timeLinePost.data?.map((post) => (
-                <div key={post.id}>
-                    <p className="text-white">{post.text}</p>
-                    <p>Post made by {post.authorName}</p>
-                </div>
-            ))}
-        </div>
-    )
+  const timeLinePost = api.user.getFeed.useQuery();
+  return (
+    <div>
+      {timeLinePost.data?.map((post) => {
+        const [firstName] = String(post.authorName).split(" ");
+        return (
+          <div key={post.id}>
+            <p className="text-white">{post.text}</p>
+            <p>Post made by {firstName}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default TimeLine
+export default TimeLine;
