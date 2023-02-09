1eab0e73187d242692b96b56e28af96ba93ea2f0
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-02-09 15:25:47 -0500

    removed Container component

--- src/pages/components/Container.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Container = ({ children, className }: { children: React.ReactNode, className: string }) => {
-  return <div className={`${className}`}>{children}</div>;
-};
-
-export default Container;
