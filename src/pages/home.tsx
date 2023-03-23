f853094de3c1ec1e2a4bf015328dfc5e1f361712
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-03-23 18:23:59 -0400

    railway db migration to planetscale scalable db migration

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -22,20 +22,20 @@ const Home: NextPage = () => {
 
 export default Home;
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const session = await getSession(context);
-//   if (!session) {
-//     return {
-//       redirect: {
-//         destination: "/login",
-//         permanent: false,
-//       },
-//     };
-//   }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
 
-//   return {
-//     props: {
-//       // user: session?.user ? session?.user : null,
-//     },
-//   };
-// };
+  return {
+    props: {
+      // user: session?.user ? session?.user : null,
+    },
+  };
+};
