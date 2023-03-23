d2ac2f90000e664c90cd71498d1dfdc0affa8d2f
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-03-22 22:31:58 -0400

    routes

---
+++ src/pages/login.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+type Props = unknown;
+
+const login = (props: Props) => {
+  return (
+    <>
+      <Navbar />
+      <div>login</div>
+    </>
+  );
+};
+
+export default login;
