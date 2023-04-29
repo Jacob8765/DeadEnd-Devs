53b48e01e33d77fa6a20ade801e9c8680760dc54
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-04-29 12:45:01 -0400

    removed hooks from timeline feed

--- src/pages/components/TimeLineFeed.tsx
+++ src/pages/components/TimeLineFeed.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React from "react";
 import { api } from "../../utils/api";
 import LoadingSpinner from "./LoadingSpinner";
 import {
